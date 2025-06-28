8e739ed83181f73e135ad281591ac32e02170fa9
gellar <gellar@tutanota.com>
2025-06-28 16:57:53 +0800

    Merge migration heads

---
+++ backend/migrations/versions/8c768b353f4b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8c768b353f4b
+Revises: a1b2c3d4e5f6, add_title_field_to_future_plans
+Create Date: 2025-06-28 16:57:20.445407
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8c768b353f4b'
+down_revision = ('a1b2c3d4e5f6', 'add_title_field_to_future_plans')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
